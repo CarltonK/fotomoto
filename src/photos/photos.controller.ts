c0536724ef437cf7091125163dde8414234e3797
Mark Carlton <mcarlton33@gmail.com>
2025-01-19 10:39:12 +0300

    discard bloat endpoints

--- src/photos/photos.controller.ts
+++ src/photos/photos.controller.ts
@@ -9,7 +9,6 @@ import {
 } from '@nestjs/common';
 import { PhotosService } from './photos.service';
 import { CreatePhotoDto } from './dto/create-photo.dto';
-import { UpdatePhotoDto } from './dto/update-photo.dto';
 
 @Controller('photos')
 export class PhotosController {
@@ -18,25 +17,5 @@ export class PhotosController {
   @Post()
   create(@Body() createPhotoDto: CreatePhotoDto) {
     return this.photosService.create(createPhotoDto);
-  }
-
-  @Get()
-  findAll() {
-    return this.photosService.findAll();
-  }
-
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.photosService.findOne(+id);
-  }
-
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updatePhotoDto: UpdatePhotoDto) {
-    return this.photosService.update(+id, updatePhotoDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.photosService.remove(+id);
   }
 }
