734476e37476e08ce18ded224b2e467d8ae2213c
Mark Carlton <mcarlton33@gmail.com>
2025-01-19 10:39:28 +0300

    import config to fix tests

--- src/firebase/firebase.module.ts
+++ src/firebase/firebase.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { FirebaseAdminService } from './firebase-admin.service';
 import { FirebaseClientService } from './firebase-client.service';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
+  imports: [ConfigModule],
   providers: [FirebaseAdminService, FirebaseClientService],
   exports: [FirebaseAdminService, FirebaseClientService],
 })
