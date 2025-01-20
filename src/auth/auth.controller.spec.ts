7d79afe28835ced711d1a47de28e2d404d1919bf
Mark Carlton <mcarlton33@gmail.com>
2025-01-20 11:33:43 +0300

    test setup

--- src/auth/auth.controller.spec.ts
+++ src/auth/auth.controller.spec.ts
@@ -3,13 +3,15 @@ import { AuthController } from './auth.controller';
 import { FirebaseModule } from './../firebase/firebase.module';
 import { AuthService } from './auth.service';
 import { mockLoggerProvider } from './../utils/test/mock-logger';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './../users/entities/user.entity';
 
 describe('AuthController', () => {
   let controller: AuthController;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      imports: [FirebaseModule],
+      imports: [FirebaseModule, TypeOrmModule.forFeature([User])],
       controllers: [AuthController],
       providers: [mockLoggerProvider, AuthService],
     }).compile();
--- src/auth/auth.service.spec.ts
+++ src/auth/auth.service.spec.ts
@@ -1,13 +1,15 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthService } from './auth.service';
 import { FirebaseModule } from './../firebase/firebase.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './../users/entities/user.entity';
 
 describe('AuthService', () => {
   let service: AuthService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      imports: [FirebaseModule],
+      imports: [FirebaseModule, TypeOrmModule.forFeature([User])],
       providers: [AuthService],
     }).compile();
 
